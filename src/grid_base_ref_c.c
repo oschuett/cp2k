9c34a6f4ad119d9662ede2cd6c13b97a9619fed0
Ole Schütt <ole.schuett@cp2k.org>
2019-09-29 12:06:17 +0200

    Make pol_x also a 3D array to unify things

--- src/grid_base_ref_c.c
+++ src/grid_base_ref_c.c
@@ -15,7 +15,7 @@ int grid_collocate_core(
                         const int lp,
                         const int cmax,
                         const double coef_xyz[(lp+1)*(lp+2)*(lp+3)/6],
-                        const double pol_x[2*cmax+1][lp+1],
+                        const double pol_x[cmax+1][lp+1][2],
                         const double pol_y[cmax+1][lp+1][2],
                         const double pol_z[cmax+1][lp+1][2],
                         const int map[3][2*cmax+1],
@@ -44,7 +44,6 @@ int grid_collocate_core(
             int lxy = 0;
             for (int lyp=0; lyp <= lp-lzp; lyp++) {
                 for (int lxp=0; lxp <= lp-lzp-lyp; lxp++) {
-                    //printf("OLE lxyz: %i, lxy: %i\n", lxyz, lxy);
                     coef_xy[lxy][0] += coef_xyz[lxyz] * pol_z[kg+cmax][lzp][0];
                     coef_xy[lxy][1] += coef_xyz[lxyz] * pol_z[kg+cmax][lzp][1];
                     lxyz++;
@@ -82,22 +81,39 @@ int grid_collocate_core(
                 }
             }
 
-            for (int ig=igmin; ig<=igmax; ig++) {
+            for (int ig=igmin; ig<=0; ig++) {
+                const int ig2 = 1 - ig;
                 const int i = map[0][ig + cmax];
+                const int i2 = map[0][ig2 + cmax];
+
                 double s01 = 0.0;
                 double s02 = 0.0;
                 double s03 = 0.0;
                 double s04 = 0.0;
+                double s05 = 0.0;
+                double s06 = 0.0;
+                double s07 = 0.0;
+                double s08 = 0.0;
+
                 for (int lxp=0; lxp <= lp; lxp++) {
-                    s01 += coef_x[lxp][0]*pol_x[ig+cmax][lxp];
-                    s02 += coef_x[lxp][1]*pol_x[ig+cmax][lxp];
-                    s03 += coef_x[lxp][2]*pol_x[ig+cmax][lxp];
-                    s04 += coef_x[lxp][3]*pol_x[ig+cmax][lxp];
+                    s01 += coef_x[lxp][0]*pol_x[ig+cmax][lxp][0];
+                    s02 += coef_x[lxp][1]*pol_x[ig+cmax][lxp][0];
+                    s03 += coef_x[lxp][2]*pol_x[ig+cmax][lxp][0];
+                    s04 += coef_x[lxp][3]*pol_x[ig+cmax][lxp][0];
+                    s05 += coef_x[lxp][0]*pol_x[ig+cmax][lxp][1];
+                    s06 += coef_x[lxp][1]*pol_x[ig+cmax][lxp][1];
+                    s07 += coef_x[lxp][2]*pol_x[ig+cmax][lxp][1];
+                    s08 += coef_x[lxp][3]*pol_x[ig+cmax][lxp][1];
                 }
+
                 grid[k-grid_lbound_z][j-grid_lbound_y][i-grid_lbound_x] += s01;
-                grid[k-grid_lbound_z][j2-grid_lbound_y][i-grid_lbound_x] += s03;
                 grid[k2-grid_lbound_z][j-grid_lbound_y][i-grid_lbound_x] += s02;
+                grid[k-grid_lbound_z][j2-grid_lbound_y][i-grid_lbound_x] += s03;
                 grid[k2-grid_lbound_z][j2-grid_lbound_y][i-grid_lbound_x] += s04;
+                grid[k-grid_lbound_z][j-grid_lbound_y][i2-grid_lbound_x] += s05;
+                grid[k2-grid_lbound_z][j-grid_lbound_y][i2-grid_lbound_x] += s06;
+                grid[k-grid_lbound_z][j2-grid_lbound_y][i2-grid_lbound_x] += s07;
+                grid[k2-grid_lbound_z][j2-grid_lbound_y][i2-grid_lbound_x] += s08;
             }
         }
     }
