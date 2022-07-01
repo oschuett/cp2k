a59b86baceec408837e7b9cfef4d5e8aee977819
Ole Schütt <ole.schuett@cp2k.org>
2022-07-01 23:14:44 +0200

    WIP

--- src/dbm/dbm_multiply_gpu.cu
+++ src/dbm/dbm_multiply_gpu.cu
@@ -52,140 +52,191 @@ __device__ static void atomicAddDouble(double *address, double val) {
 #endif
 }
 
-#define TILE_DIM 8
+#define TILE_DIM 2
 
-#define ELEMENTS_PER_THREAD 4
+//#define ELEMENTS_PER_THREAD 4
 #define NUM_THREADS 256
 
-/*******************************************************************************
- * \brief A generic matrix multiplication kernel.
- * \author Ole Schuett
- ******************************************************************************/
-__device__ static void
-process_batch_kernel_old(const dbm_task_t task, const double alpha,
-                         const double *pack_a_data, const double *pack_b_data,
-                         double *shard_c_data, double *shared_memory) {
+// /*******************************************************************************
+//  * \brief A generic matrix multiplication kernel.
+//  * \author Ole Schuett
+//  ******************************************************************************/
+// __device__ static void
+// process_batch_kernel_old(const dbm_task_t task, const double alpha,
+//                          const double *pack_a_data, const double *pack_b_data,
+//                          double *shard_c_data, double *shared_memory) {
+// 
+//   const double *block_a = &pack_a_data[task.offset_a];
+//   const double *block_b = &pack_b_data[task.offset_b];
+//   double *block_c = &shard_c_data[task.offset_c];
+// 
+//   double *tile_a = shared_memory;
+//   double *tile_b = &shared_memory[TILE_DIM * TILE_DIM];
+// 
+//   for (int i_tile = 0; i_tile < task.m; i_tile += TILE_DIM) {
+//     for (int j_tile = 0; j_tile < task.n; j_tile += TILE_DIM) {
+//       double result = 0.0;
+//       for (int l_tile = 0; l_tile < task.k; l_tile += TILE_DIM) {
+//         // Map indicies to threads such that memory reads are coalesced.
+//         const int i = i_tile + threadIdx.x;
+//         const int j = j_tile + threadIdx.x; // Different from j in final loop!
+//         const int l = l_tile + threadIdx.y;
+// 
+//         // Load tile_a from global into shared memory.
+//         const int idx_a = l * task.m + i; // transa = "N"
+//         const bool load_a = (l < task.k && i < task.m);
+//         tile_a[threadIdx.y * TILE_DIM + threadIdx.x] =
+//             (load_a) ? block_a[idx_a] : 0.0;
+// 
+//         // Load tile_b from global into shared memory.
+//         const int idx_b = l * task.n + j; // transb = "T"
+//         const bool load_b = (l < task.k && j < task.n);
+//         tile_b[threadIdx.y * TILE_DIM + threadIdx.x] =
+//             (load_b) ? block_b[idx_b] : 0.0;
+// 
+//         // Multiply tiles from shared memory.
+//         __syncthreads();
+// #pragma unroll
+//         for (int z = 0; z < TILE_DIM; z++) {
+//           result += tile_a[z * TILE_DIM + threadIdx.x] *
+//                     tile_b[z * TILE_DIM + threadIdx.y];
+//         }
+//         __syncthreads();
+//       }
+// 
+//       // Add result tile to block_c in global memory.
+//       const int i = i_tile + threadIdx.x;
+//       const int j = j_tile + threadIdx.y; // Different from j in inner loop!
+//       if (i < task.m && j < task.n) {
+//         const int idx_c = j * task.m + i;
+//         // Need atomics because other thread blocks might work on same block_c.
+//         atomicAddDouble(&block_c[idx_c], alpha * result);
+//       }
+//     }
+//   }
+// }
 
-  const double *block_a = &pack_a_data[task.offset_a];
-  const double *block_b = &pack_b_data[task.offset_b];
-  double *block_c = &shard_c_data[task.offset_c];
-
-  double *tile_a = shared_memory;
-  double *tile_b = &shared_memory[TILE_DIM * TILE_DIM];
-
-  for (int i_tile = 0; i_tile < task.m; i_tile += TILE_DIM) {
-    for (int j_tile = 0; j_tile < task.n; j_tile += TILE_DIM) {
-      double result = 0.0;
-      for (int l_tile = 0; l_tile < task.k; l_tile += TILE_DIM) {
-        // Map indicies to threads such that memory reads are coalesced.
-        const int i = i_tile + threadIdx.x;
-        const int j = j_tile + threadIdx.x; // Different from j in final loop!
-        const int l = l_tile + threadIdx.y;
-
-        // Load tile_a from global into shared memory.
-        const int idx_a = l * task.m + i; // transa = "N"
-        const bool load_a = (l < task.k && i < task.m);
-        tile_a[threadIdx.y * TILE_DIM + threadIdx.x] =
-            (load_a) ? block_a[idx_a] : 0.0;
-
-        // Load tile_b from global into shared memory.
-        const int idx_b = l * task.n + j; // transb = "T"
-        const bool load_b = (l < task.k && j < task.n);
-        tile_b[threadIdx.y * TILE_DIM + threadIdx.x] =
-            (load_b) ? block_b[idx_b] : 0.0;
-
-        // Multiply tiles from shared memory.
-        __syncthreads();
-#pragma unroll
-        for (int z = 0; z < TILE_DIM; z++) {
-          result += tile_a[z * TILE_DIM + threadIdx.x] *
-                    tile_b[z * TILE_DIM + threadIdx.y];
-        }
-        __syncthreads();
-      }
-
-      // Add result tile to block_c in global memory.
-      const int i = i_tile + threadIdx.x;
-      const int j = j_tile + threadIdx.y; // Different from j in inner loop!
-      if (i < task.m && j < task.n) {
-        const int idx_c = j * task.m + i;
-        // Need atomics because other thread blocks might work on same block_c.
-        atomicAddDouble(&block_c[idx_c], alpha * result);
-      }
-    }
-  }
-}
-/*******************************************************************************
- * \brief A generic matrix multiplication kernel.
- * \author Ole Schuett
- ******************************************************************************/
-__device__ static void
-process_batch_kernel_new(const dbm_task_t task, const double alpha,
-                         const double *pack_a_data, const double *pack_b_data,
-                         double *shard_c_data, double *shared_memory) {
-  /* Total number of elements in block matrices */
-  const int mk = task.m * task.k; /* a_block */
-  const int kn = task.n * task.k; /* b_block */
-  const int mn = task.m * task.n; /* c_block */
-
-  const double *data_a = &pack_a_data[task.offset_a];
-  const double *data_b = &pack_b_data[task.offset_b];
-  double *data_c = &shard_c_data[task.offset_c];
-
-  double *buffer_a = shared_memory;
-  double *buffer_b = &shared_memory[mk];
-
-  // Load tile_a from global into shared memory.
-  for (int i = threadIdx.x; i < mk; i += blockDim.x) {
-    buffer_a[i] = __ldg(&data_a[i]);
-  }
-
-  // Load tile_b from global into shared memory.
-  for (int i = threadIdx.x; i < kn; i += blockDim.x) {
-    buffer_b[i] = __ldg(&data_b[i]);
-  }
-
-  double tile_c[ELEMENTS_PER_THREAD] = {1.0};
-
-  __syncthreads();
-  //#pragma unroll
-  //  for (int z = 0; z < ELEMENTS_PER_THREAD; z++) {
-  //    const int ij = threadIdx.x * ELEMENTS_PER_THREAD + z;
-  //    if (ij < mn) {
-  //      const int i = ij / task.m; // TODO this might be too expensive.
-  //      const int j = ij - task.m*i;
-  //      for (int l = 0; l < task.k; l++) {
-  //        /* Compute c_ij = sum_k (a_ik * b_kj) in shared memory */
-  //        tile_c[z] += buffer_a[l * task.m + j] * buffer_b[l * task.n + i];
-  //      }
-  //    }
-  //  }
-
-#define N 2
-#define M 2
-  for (int l = 0; l < task.k; l++) {
-#pragma unroll
-    for (int i = 0; i < N; i++) {
-#pragma unroll
-      for (int j = 0; j < M; j++) {
-        tile_c[M * i + j] +=
-            buffer_a[l * task.m + j] * buffer_b[l * task.n + i];
-      }
-    }
-  }
-
-  // TODO maybe need __syncthreads(); later.
-
-  // Add result tile to block_c in global memory.
-#pragma unroll
-  for (int z = 0; z < ELEMENTS_PER_THREAD; z++) {
-    const int ij = threadIdx.x * ELEMENTS_PER_THREAD + z;
-    if (ij < mn) {
-      // Need atomics because other thread blocks might work on same block_c.
-      atomicAddDouble(&data_c[ij], alpha * tile_c[z]);
-    }
-  }
-}
+///*******************************************************************************
+// * \brief A generic matrix multiplication kernel.
+// * \author Ole Schuett
+// ******************************************************************************/
+//__device__ static void
+//process_batch_kernel_new(const dbm_task_t task, const double alpha,
+//                         const double *pack_a_data, const double *pack_b_data,
+//                         double *shard_c_data, double *shared_memory) {
+//  /* Total number of elements in block matrices */
+//  const int mk = task.m * task.k; /* a_block */
+//  const int kn = task.n * task.k; /* b_block */
+//  const int mn = task.m * task.n; /* c_block */
+//
+//    /* Number of columns and rows used to divide c_block into tiles */
+//  const int ntile_cols = (task.n + TILE_DIM -1) / TILE_DIM;
+//  const int ntile_rows = (task.m + TILE_DIM -1) / TILE_DIM;
+//
+//  /* Column and row starting index of the tile T this thread will compute
+//   * Each thread computes all elements of one tile T */
+//  const int tile_col = threadIdx.x / ntile_rows;
+//  const int tile_row = threadIdx.x - tile_col * ntile_rows;
+//
+//  const double *data_a = &pack_a_data[task.offset_a];
+//  const double *data_b = &pack_b_data[task.offset_b];
+//  double *data_c = &shard_c_data[task.offset_c];
+//
+//  double *buffer_a = shared_memory;
+//  double *buffer_b = &shared_memory[mk];
+//
+//  double tile_c[TILE_DIM*TILE_DIM] = {1.0};
+//
+//  // Load tile_a from global into shared memory.
+//  for (int i = threadIdx.x; i < mk; i += blockDim.x) {
+//    buffer_a[i] = __ldg(&data_a[i]);
+//  }
+//
+//  // Load tile_b from global into shared memory.
+//  for (int i = threadIdx.x; i < kn; i += blockDim.x) {
+//    buffer_b[i] = __ldg(&data_b[i]);
+//  }
+//
+//
+//  __syncthreads();
+//  /* Do actual multiplication. */
+//  if (tile_col < ntile_cols && tile_row < ntile_rows) {
+//    for (int l = 0; l < task.k; l++) {
+//#pragma unroll
+//      for (int i = 0; i < TILE_DIM; i++) {
+//#pragma unroll
+//        for (int j = 0; j < TILE_DIM; j++) {
+//          const int idx_a = l * task.m + tile_row * TILE_DIM +  j;
+//          const int idx_b = l * task.n + tile_col * TILE_DIM  + i;
+//          tile_c[i * TILE_DIM + j] += buffer_a[idx_a]  * buffer_b[idx_b];
+//        }
+//      }
+//    }
+//  }
+//
+//
+//  __syncthreads();
+//
+//  /* Decompress results to buffer and set tile elements back to 0 */
+//  if (tile_col < ntile_cols && tile_row < ntile_rows) {
+//#pragma unroll
+//    for (int i = 0; i < TILE_DIM; i++) {
+//#pragma unroll
+//       for (int j = 0; j < TILE_DIM; j++) {  
+//         const int idx_a = tile_row * TILE_DIM  + j;
+//         const int idx_b = tile_col * TILE_DIM  + i;
+//         if (idx_a < task.m && idx_b < task.n) {
+//           shared_memory[idx_b* task.m + idx_a] = tile_c[i * TILE_DIM + j];
+//         }
+//       }
+//     }
+//  }
+//  __syncthreads();
+//
+//   /* Add results from shared memory buffer to global C block. */
+//#pragma unroll
+//  for (int i = threadIdx.x; i < mn; i +=  blockDim.x) {
+//    atomicAdd(&data_c[i], shared_memory[i]);
+//  }
+//  __syncthreads();
+//
+////#pragma unroll
+////  for (int z = 0; z < ELEMENTS_PER_THREAD; z++) {
+////    const int ij = threadIdx.x * ELEMENTS_PER_THREAD + z;
+////    if (ij < mn) {
+////      const int i = ij / task.m; // TODO this might be too expensive.
+////      const int j = ij - task.m*i;
+////      for (int l = 0; l < task.k; l++) {
+////        /* Compute c_ij = sum_k (a_ik * b_kj) in shared memory */
+////        tile_c[z] += buffer_a[l * task.m + j] * buffer_b[l * task.n + i];
+////      }
+////    }
+////  }
+////
+////
+////  for (int l = 0; l < task.k; l++) {
+////#pragma unroll
+////    for (int i = 0; i < TILE_DIM; i++) {
+////#pragma unroll
+////      for (int j = 0; j < TILE_DIM; j++) {
+////        tile_c[M * i + j] +=
+////            buffer_a[l * task.m + j] * buffer_b[l * task.n + i];
+////      }
+////    }
+////  }
+////
+////  // TODO maybe need __syncthreads(); later.
+//
+//  // Add result tile to block_c in global memory.
+////#pragma unroll
+////  for (int z = 0; z < TILE_DIM*TILE_DIM; z++) {
+////    const int ij = threadIdx.x * ELEMENTS_PER_THREAD + z;
+////    if (ij < mn) {
+////      // Need atomics because other thread blocks might work on same block_c.
+////      atomicAddDouble(&data_c[ij], alpha * tile_c[z]);
+////    }
+////  }
+//}
 
 /*******************************************************************************
  * \brief A generic matrix multiplication kernel.
@@ -197,6 +248,8 @@ __global__ static void process_batch_kernel(const double alpha,
                                             const double *pack_b_data,
                                             double *shard_c_data) {
 
+  __shared__ double shared_memory[2 * TILE_DIM * TILE_DIM * NUM_THREADS];
+
   const dbm_task_t task = batch[blockIdx.x]; // TODO load via shared memory
 
   /* Total number of elements in block matrices */
@@ -204,13 +257,79 @@ __global__ static void process_batch_kernel(const double alpha,
   const int kn = task.n * task.k; /* b_block */
   const int mn = task.m * task.n; /* c_block */
 
-  assert(mk + kn <= 2 * NUM_THREADS * ELEMENTS_PER_THREAD);
-  assert(mn <= NUM_THREADS * ELEMENTS_PER_THREAD);
+  assert(mk + kn <= 2 * NUM_THREADS * TILE_DIM * TILE_DIM);
+  assert(mn <= NUM_THREADS * TILE_DIM * TILE_DIM);
 
-  __shared__ double shared_memory[2 * ELEMENTS_PER_THREAD * NUM_THREADS];
+  /* Number of columns and rows used to divide c_block into tiles */
+  const int ntile_cols = (task.n + TILE_DIM -1) / TILE_DIM;
+  const int ntile_rows = (task.m + TILE_DIM -1) / TILE_DIM;
 
-  process_batch_kernel_new(task, alpha, pack_a_data, pack_b_data, shard_c_data,
-                           shared_memory);
+  /* Column and row starting index of the tile T this thread will compute
+   * Each thread computes all elements of one tile T */
+  const int tile_col = threadIdx.x / ntile_rows;
+  const int tile_row = threadIdx.x - tile_col * ntile_rows;
+
+  const double *data_a = &pack_a_data[task.offset_a];
+  const double *data_b = &pack_b_data[task.offset_b];
+  double *data_c = &shard_c_data[task.offset_c];
+
+  double *buffer_a = shared_memory;
+  double *buffer_b = &shared_memory[mk];
+
+  double tile_c[TILE_DIM*TILE_DIM] = {1.0};
+
+  // Load tile_a from global into shared memory.
+  for (int i = threadIdx.x; i < mk; i += blockDim.x) {
+    buffer_a[i] = __ldg(&data_a[i]);
+  }
+
+  // Load tile_b from global into shared memory.
+  for (int i = threadIdx.x; i < kn; i += blockDim.x) {
+    buffer_b[i] = __ldg(&data_b[i]);
+  }
+
+
+  __syncthreads();
+  /* Do actual multiplication. */
+  if (tile_col < ntile_cols && tile_row < ntile_rows) {
+    for (int l = 0; l < task.k; l++) {
+#pragma unroll
+      for (int i = 0; i < TILE_DIM; i++) {
+#pragma unroll
+        for (int j = 0; j < TILE_DIM; j++) {
+          const int idx_a = l * task.m + tile_row * TILE_DIM +  j;
+          const int idx_b = l * task.n + tile_col * TILE_DIM  + i;
+          tile_c[i * TILE_DIM + j] += buffer_a[idx_a]  * buffer_b[idx_b];
+        }
+      }
+    }
+  }
+
+
+  __syncthreads();
+
+  /* Decompress results to buffer and set tile elements back to 0 */
+  if (tile_col < ntile_cols && tile_row < ntile_rows) {
+#pragma unroll
+    for (int i = 0; i < TILE_DIM; i++) {
+#pragma unroll
+       for (int j = 0; j < TILE_DIM; j++) {  
+         const int idx_a = tile_row * TILE_DIM  + j;
+         const int idx_b = tile_col * TILE_DIM  + i;
+         if (idx_a < task.m && idx_b < task.n) {
+           shared_memory[idx_b* task.m + idx_a] = tile_c[i * TILE_DIM + j];
+         }
+       }
+     }
+  }
+  __syncthreads();
+
+   /* Add results from shared memory buffer to global C block. */
+#pragma unroll
+  for (int i = threadIdx.x; i < mn; i +=  blockDim.x) {
+    atomicAdd(&data_c[i], shared_memory[i]);
+  }
+
 }
 
 /*******************************************************************************
