85505ba85153db90c011f2f25c68bc100f0cda24
Ole Schütt <ole.schuett@cp2k.org>
2022-07-02 22:55:58 +0200

    DBM: Use more register in GPU kernel

--- src/dbm/dbm_multiply_gpu.cu
+++ src/dbm/dbm_multiply_gpu.cu
@@ -18,6 +18,8 @@
 #include <stdio.h>
 
 #define NUM_THREADS 256
+#define NUM_REGISTERS 2
+#define NUM_REGISTERS_SQUARED (NUM_REGISTERS * NUM_REGISTERS)
 
 /*******************************************************************************
  * \brief Atomic add for doubles that also works prior to compute capability 6.
@@ -52,6 +54,7 @@ __device__ static void atomicAddDouble(double *address, double val) {
 __device__ constexpr static inline int imax(int x, int y) {
   return (x > y ? x : y);
 }
+
 /*******************************************************************************
  * \brief A generic matrix multiplication kernel.
  * \author Ole Schuett
@@ -62,65 +65,97 @@ __device__ static void process_task(const int m, const int n, const int k,
                                     const double *block_b, double *block_c,
                                     double *shared_mem) {
 
-  constexpr int K = NUM_THREADS / imax(M, N);
+  constexpr int K = (NUM_THREADS * NUM_REGISTERS_SQUARED) / imax(M, N);
 
-  static_assert(K * imax(M, N) == NUM_THREADS, "Wasting threads.");
-  static_assert(M * N <= NUM_THREADS, "Not enough threads to cover tile.");
+  static_assert(M * N <= NUM_THREADS * NUM_REGISTERS_SQUARED,
+                "Not enought threads to cover tile.");
+  static_assert(K * imax(M, N) == NUM_THREADS * NUM_REGISTERS_SQUARED,
+                "Wasting threads.");
+  static_assert((M / NUM_REGISTERS) * NUM_REGISTERS == M,
+                "M not divisible by NUM_REGISTERS");
+  static_assert((N / NUM_REGISTERS) * NUM_REGISTERS == N,
+                "N not divisible by NUM_REGISTERS");
 
   double *tile_a = shared_mem;
   double *tile_b = &shared_mem[K * M];
 
   // Layout threads to cover the entire tile.
   // Indices x,y,z mark the position within the tile.
-  const int y = threadIdx.x / M;       // Can exceed N, guaranteed to reach K.
-  const int x = threadIdx.x - y * M;   // Fastest index, does not exceed M.
-  const int xT = threadIdx.x / N;      // Can exceed M, guaranteed to reach K.
-  const int yT = threadIdx.x - xT * N; // Fastest index, does not exceed N.
+  const int register_idx = (threadIdx.x * NUM_REGISTERS_SQUARED);
+  const int y = register_idx / M;       // Can exceed N, guaranteed to reach K.
+  const int x = register_idx - y * M;   // Fastest index, does not exceed M.
+  const int xT = register_idx / N;      // Can exceed M, guaranteed to reach K.
+  const int yT = register_idx - xT * N; // Fastest index, does not exceed N.
 
   // Indices {ijl}_tile mark the beginning of the current tile.
   for (int i_tile = 0; i_tile < m; i_tile += M) {
     for (int j_tile = 0; j_tile < n; j_tile += N) {
-      double result = 0.0;
+      double result[NUM_REGISTERS][NUM_REGISTERS] = {0.0};
       for (int l_tile = 0; l_tile < k; l_tile += K) {
 
         // Load tile_a from global into shared memory.
-        if (x < M && y < K) {
-          const int i = i_tile + x;
-          const int l = l_tile + y;
-          const int idx = l * m + i; // transa = "N"
-          const bool load = (l < k && i < m);
-          tile_a[y * M + x] = (load) ? block_a[idx] : 0.0;
+        if (x + NUM_REGISTERS < M && y + NUM_REGISTERS < K) {
+#pragma unroll
+          for (int ry = 0; ry < NUM_REGISTERS; ry++) {
+#pragma unroll
+            for (int rx = 0; rx < NUM_REGISTERS; rx++) {
+              const int i = i_tile + x + rx;
+              const int l = l_tile + y + ry;
+              const int idx = l * m + i; // transa = "N"
+              const bool load = (l < k && i < m);
+              tile_a[(y + ry) * M + x + rx] = (load) ? block_a[idx] : 0.0;
+            }
+          }
         }
 
         // Load tile_b from global into shared memory.
         // Use transposed thread mapping to achieve coalesced memory reads.
-        if (yT < N && xT < K) {
-          const int j = j_tile + yT;
-          const int l = l_tile + xT;
-          const int idx = l * n + j; // transb = "T"
-          const bool load = (l < k && j < n);
-          tile_b[xT * N + yT] = (load) ? block_b[idx] : 0.0;
+        if (yT + NUM_REGISTERS < N && xT + NUM_REGISTERS < K) {
+#pragma unroll
+          for (int rx = 0; rx < NUM_REGISTERS; rx++) {
+#pragma unroll
+            for (int ry = 0; ry < NUM_REGISTERS; ry++) {
+              const int j = j_tile + yT + ry;
+              const int l = l_tile + xT + rx;
+              const int idx = l * n + j; // transb = "T"
+              const bool load = (l < k && j < n);
+              tile_b[(xT + rx) * N + yT + ry] = (load) ? block_b[idx] : 0.0;
+            }
+          }
         }
 
         // Multiply tiles from shared memory.
         __syncthreads();
-        if (x < M && y < N) {
+        if (x + NUM_REGISTERS < M && y + NUM_REGISTERS < N) {
 #pragma unroll
           for (int z = 0; z < K; z++) {
-            result += tile_a[z * M + x] * tile_b[z * N + y];
+#pragma unroll
+            for (int ry = 0; ry < NUM_REGISTERS; ry++) {
+#pragma unroll
+              for (int rx = 0; rx < NUM_REGISTERS; rx++) {
+                result[rx][ry] +=
+                    tile_a[z * M + x + rx] * tile_b[z * N + y + ry];
+              }
+            }
           }
         }
         __syncthreads();
       }
 
       // Add result tile to block_c in global memory.
-      if (x < M && y < N) {
-        const int i = i_tile + x;
-        const int j = j_tile + y;
-        if (i < m && j < n) {
-          const int idx = j * m + i;
-          // Need atomics because other thread blocks might work on same C.
-          atomicAddDouble(&block_c[idx], alpha * result);
+      if (x + NUM_REGISTERS < M && y + NUM_REGISTERS < N) {
+#pragma unroll
+        for (int ry = 0; ry < NUM_REGISTERS; ry++) {
+#pragma unroll
+          for (int rx = 0; rx < NUM_REGISTERS; rx++) {
+            const int i = i_tile + x + rx;
+            const int j = j_tile + y + ry;
+            if (i < m && j < n) {
+              const int idx_c = j * m + i;
+              // Needs atomics because other thread blocks might work on same C.
+              atomicAddDouble(&block_c[idx_c], alpha * result[rx][ry]);
+            }
+          }
         }
       }
     }
@@ -137,7 +172,7 @@ __global__ static void process_batch_kernel(const double alpha,
                                             const double *pack_b_data,
                                             double *shard_c_data) {
 
-  __shared__ double shared_mem[2 * NUM_THREADS];
+  __shared__ double shared_mem[2 * NUM_REGISTERS_SQUARED * NUM_THREADS];
 
   const dbm_task_t task = batch[blockIdx.x];
 
@@ -149,51 +184,53 @@ __global__ static void process_batch_kernel(const double alpha,
   const double *blk_b = &pack_b_data[task.offset_b];
   double *blk_c = &shard_c_data[task.offset_c];
 
-  if (m <= 4 && n <= 4) {
-    process_task<4, 4>(m, n, k, alpha, blk_a, blk_b, blk_c, shared_mem);
+  process_task<32, 32>(m, n, k, alpha, blk_a, blk_b, blk_c, shared_mem);
 
-  } else if (m <= 4 && n <= 8) {
-    process_task<4, 8>(m, n, k, alpha, blk_a, blk_b, blk_c, shared_mem);
-
-  } else if (m <= 4 && n <= 16) {
-    process_task<4, 16>(m, n, k, alpha, blk_a, blk_b, blk_c, shared_mem);
-
-  } else if (m <= 4 && n <= 32) {
-    process_task<4, 32>(m, n, k, alpha, blk_a, blk_b, blk_c, shared_mem);
-
-  } else if (m <= 4) {
-    process_task<4, 64>(m, n, k, alpha, blk_a, blk_b, blk_c, shared_mem);
-
-  } else if (m <= 8 && n <= 4) {
-    process_task<8, 4>(m, n, k, alpha, blk_a, blk_b, blk_c, shared_mem);
-
-  } else if (m <= 16 && n <= 4) {
-    process_task<16, 4>(m, n, k, alpha, blk_a, blk_b, blk_c, shared_mem);
-
-  } else if (m <= 32 && n <= 4) {
-    process_task<32, 4>(m, n, k, alpha, blk_a, blk_b, blk_c, shared_mem);
-
-  } else if (n <= 4) {
-    process_task<64, 4>(m, n, k, alpha, blk_a, blk_b, blk_c, shared_mem);
-
-  } else if (m <= 8 && n <= 8) {
-    process_task<8, 8>(m, n, k, alpha, blk_a, blk_b, blk_c, shared_mem);
-
-  } else if (m <= 8 && n <= 16) {
-    process_task<8, 16>(m, n, k, alpha, blk_a, blk_b, blk_c, shared_mem);
-
-  } else if (m <= 8) {
-    process_task<8, 32>(m, n, k, alpha, blk_a, blk_b, blk_c, shared_mem);
-
-  } else if (m <= 16 && n <= 8) {
-    process_task<16, 8>(m, n, k, alpha, blk_a, blk_b, blk_c, shared_mem);
-
-  } else if (n <= 8) {
-    process_task<32, 8>(m, n, k, alpha, blk_a, blk_b, blk_c, shared_mem);
-
-  } else {
-    process_task<16, 16>(m, n, k, alpha, blk_a, blk_b, blk_c, shared_mem);
-  }
+  // if (m <= 4 && n <= 4) {
+  //   process_task<4, 4>(m, n, k, alpha, blk_a, blk_b, blk_c, shared_mem);
+  //
+  // } else if (m <= 4 && n <= 8) {
+  //   process_task<4, 8>(m, n, k, alpha, blk_a, blk_b, blk_c, shared_mem);
+  //
+  // } else if (m <= 4 && n <= 16) {
+  //   process_task<4, 16>(m, n, k, alpha, blk_a, blk_b, blk_c, shared_mem);
+  //
+  // } else if (m <= 4 && n <= 32) {
+  //   process_task<4, 32>(m, n, k, alpha, blk_a, blk_b, blk_c, shared_mem);
+  //
+  // } else if (m <= 4) {
+  //   process_task<4, 64>(m, n, k, alpha, blk_a, blk_b, blk_c, shared_mem);
+  //
+  // } else if (m <= 8 && n <= 4) {
+  //   process_task<8, 4>(m, n, k, alpha, blk_a, blk_b, blk_c, shared_mem);
+  //
+  // } else if (m <= 16 && n <= 4) {
+  //   process_task<16, 4>(m, n, k, alpha, blk_a, blk_b, blk_c, shared_mem);
+  //
+  // } else if (m <= 32 && n <= 4) {
+  //   process_task<32, 4>(m, n, k, alpha, blk_a, blk_b, blk_c, shared_mem);
+  //
+  // } else if (n <= 4) {
+  //   process_task<64, 4>(m, n, k, alpha, blk_a, blk_b, blk_c, shared_mem);
+  //
+  // } else if (m <= 8 && n <= 8) {
+  //   process_task<8, 8>(m, n, k, alpha, blk_a, blk_b, blk_c, shared_mem);
+  //
+  // } else if (m <= 8 && n <= 16) {
+  //   process_task<8, 16>(m, n, k, alpha, blk_a, blk_b, blk_c, shared_mem);
+  //
+  // } else if (m <= 8) {
+  //   process_task<8, 32>(m, n, k, alpha, blk_a, blk_b, blk_c, shared_mem);
+  //
+  // } else if (m <= 16 && n <= 8) {
+  //   process_task<16, 8>(m, n, k, alpha, blk_a, blk_b, blk_c, shared_mem);
+  //
+  // } else if (n <= 8) {
+  //   process_task<32, 8>(m, n, k, alpha, blk_a, blk_b, blk_c, shared_mem);
+  //
+  // } else {
+  //   process_task<16, 16>(m, n, k, alpha, blk_a, blk_b, blk_c, shared_mem);
+  // }
 }
 
 /*******************************************************************************
